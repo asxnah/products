9779ad3da07b5a969a27a55cd89f9b1893767f0c
asuna <cherepanova.so04@gmail.com>
2025-11-18 15:32:06 +0500

    refactor: app name

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import Navbar from '@/components/Navbar';
 
 export const metadata: Metadata = {
-  title: 'Next App',
+  title: 'Products app with dummyjson',
   description: 'Next App',
 };
 
